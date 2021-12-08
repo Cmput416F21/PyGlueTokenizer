6de0560f0923caa72de20d342220b92df8eee872
Kiran Deol <54760157+kirandeol@users.noreply.github.com>
2021-12-07 23:50:08 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
+cd CodeGen-main
 sh CodeGen-main/install_env.sh
+cd ..
 pip install -r requirements.txt
 wget https://dl.fbaipublicfiles.com/transcoder/pre_trained_models/translator_transcoder_size_from_DOBF.pth -P CodeGen-main
 wget https://s3.amazonaws.com/code2seq/model/java-large/java-large-model.tar.gz -P code2seq
